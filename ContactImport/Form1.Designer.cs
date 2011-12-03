11759a33e23079c9aa045d612755740f06b72089
Nicolas Galler <ngaller@gmail.com>
2011-12-03 12:16:30 -0600

    Initial

---
+++ ContactImport/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ContactImport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(116, 10);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(346, 20);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(116, 46);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(346, 20);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(379, 91);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 4;
+            this.btnGo.Text = "button1";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 153);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ ContactImport/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Google.GData.Client;
+using System.Data.OleDb;
+using Google.GData.Contacts;
+using Google.GData.Extensions;
+
+namespace ContactImport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            ImportAllContacts(txtEmail.Text, txtPassword.Text);
+            MessageBox.Show("DONE");
+        }
+
+        private static void ImportAllContacts(String username, String password)
+        {
+            Service service = new Service("cp", "exampleCo-exampleApp-1");
+            // Set your credentials:
+            service.setUserCredentials(username, password);
+            service.ProtocolMajor = 3;
+            service.ProtocolMinor = 0;
+
+            //ContactsRequest cr = new ContactsRequest();
+            OleDbConnection cn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Documents and Settings\\Administrator\\My Documents\\BlackBerry\\Backup;Extended Properties=\"text;HDR=Yes;FMT=Delimited\";");
+            cn.Open();
+            OleDbCommand cmd = cn.CreateCommand();
+            cmd.CommandText = "select * from [MB_Export_02205018.csv]";
+            using (var rdr = cmd.ExecuteReader())
+            {
+                while (rdr.Read())
+                {
+                    if (rdr[0].ToString() == "")
+                        continue;
+                    ContactEntry entry = new ContactEntry();
+                    entry.Name = new Google.GData.Extensions.Name { FullName = rdr["Name"].ToString(), GivenName = rdr["First Name"].ToString(), FamilyName = rdr["Last Name"].ToString() };
+                    entry.GroupMembership.Add(new GroupMembership { HRef = "https://www.google.com/m8/feeds/groups/" + username + "/base/6" });
+                    if (rdr["Company"].ToString() != "")
+                        entry.Organizations.Add(new Organization { Name = rdr["Company"].ToString(), Title = rdr["Job"].ToString(), Rel = "http://schemas.google.com/g/2005#work" });
+                    if (rdr["Email"].ToString() != "")
+                        entry.Emails.Add(new Google.GData.Extensions.EMail { Label = "Home", Address = rdr["Email"].ToString() });
+                    if (rdr["Mobile"].ToString() != "")
+                        entry.Phonenumbers.Add(new PhoneNumber { Label = "Mobile", Value = rdr["Mobile"].ToString() });
+                    if (rdr["Work Phone 1"].ToString() != "")
+                        entry.Phonenumbers.Add(new PhoneNumber { Label = "Work", Value = rdr["Work Phone 1"].ToString() });
+                    if (rdr["Work Phone 2"].ToString() != "")
+                        entry.Phonenumbers.Add(new PhoneNumber { Label = "Work 2", Value = rdr["Work Phone 2"].ToString() });
+                    if (rdr["Home Phone 1"].ToString() != "")
+                        entry.Phonenumbers.Add(new PhoneNumber { Label = "Home", Value = rdr["Home Phone 1"].ToString() });
+                    if (rdr["Home Address 1"].ToString() != "")
+                    {
+                        entry.PostalAddresses.Add(new StructuredPostalAddress
+                        {
+                            Label = "Home",
+                            Street = rdr["Home Address 1"].ToString(),
+                            City = rdr["Home City"].ToString(),
+                            Region = rdr["Home State"].ToString(),
+                            Postcode = rdr["Home ZIP"].ToString()
+                        });
+                    }
+                    if (rdr["Work Address 1"].ToString() != "")
+                    {
+                        entry.PostalAddresses.Add(new StructuredPostalAddress
+                        {
+                            Label = "Work",
+                            Street = rdr["Work Address 1"].ToString(),
+                            City = rdr["Work City"].ToString(),
+                            Region = rdr["Work State"].ToString(),
+                            Postcode = rdr["Work ZIP"].ToString()
+                        });
+                    }
+                    if (rdr["Birthday"].ToString() != "")
+                    {
+                        DateTime bd;
+                        if (DateTime.TryParse(rdr["Birthday"].ToString(), out bd))
+                        {
+                            entry.Birthday = bd.ToString("yyyy-MM-dd");
+                        }
+                    }
+                    Uri feedUri = new Uri(ContactsQuery.CreateContactsUri("default"));
+                    AtomEntry insertedEntry = service.Insert(feedUri, entry);
+                }
+            }
+        }
+    }
+}
